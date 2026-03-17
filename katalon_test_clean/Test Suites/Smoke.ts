18cfd1b056cbe47b03615d20580d00c348b120b5
Don Maslanka <don.maslanka@toptal.com>
2026-03-17 08:20:07 -0600

    katalon studio generated prj files

---
+++ katalon_test_clean/Test Suites/Smoke.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>91a9f7ba-ea9d-4c67-8e5b-457f7bf6fb4c</testSuiteGuid>
+   <testCaseLink>
+      <guid>7ec6ad55-0ddc-44b5-a98a-787184f488ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Jenkins2_Login_Reachable</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
